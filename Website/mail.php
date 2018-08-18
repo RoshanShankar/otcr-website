85ce6443bb3fdbf579a83ec658d6eda9b0a20b82
anandrishabh1 <anand.rishabh1@gmail.com>
2018-08-18 09:52:32 +0530

    Update mail.php

--- Website/mail.php
+++ Website/mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $recipient = "rishabh3@illinois.edu";
-$subject = $_POST['subject']
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
