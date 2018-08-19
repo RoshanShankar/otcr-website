097225a06390ca5fac2e5bbca4777eec0d7bb13a
1mihirpandya <1mihirpandya@gmail.com>
2018-08-19 02:18:55 -0700

    uploading new application processing, corresponding html form will be uploaded soon

--- Website/contactform/DriveAPI/upload_resume.php
+++ Website/contactform/DriveAPI/upload_resume.php
@@ -30,6 +30,35 @@ function getApplicationFolder($driveService)
 
 function createApplicantFolder($driveService, $parentFolderId, $studentName)
 {
+    //Create applicant file
+    $application_file = $_POST['name'].'.txt';
+    $handle = fopen($application_file, 'w') or die('Cannot open file:  '.$application_file);
+    fwrite($handle, 'Name: '.$_POST['name']);
+    fwrite($handle, "\n".'Phone Number: '.$_POST['phone_number']);
+    fwrite($handle, "\n".'Email: '.$_POST['email']);
+    fwrite($handle, "\n".'NetID: '.$_POST['netid']);
+    $checkbox = "N/A";
+    if ($_POST['quad'] == "Quad Day") {
+      $checkbox = "Quad Day";
+    } else if ($_POST['bquad'] == "Business Quad Day") {
+      $checkbox = "Business Quad Day";
+    } else if ($_POST['info_night'] == "Info Nights") {
+      $checkbox = "Info Nights";
+    } else if ($_POST['social'] == "Social Media") {
+      $checkbox = "Social Media";
+    } else if ($_POST['friend'] == "Friend") {
+      $checkbox = "Friend";
+    } else if ($_POST['other'] == "Other") {
+      $checkbox = "Other";
+    }
+    fwrite($handle, "\n".'How they heard about OTCR: '.$checkbox."\n");
+    fwrite($handle, "\n".'Major: '.$_POST['major']);
+    fwrite($handle, "\n".'Graduation Year: '.$_POST['start_year']);
+    fwrite($handle, "\n".'Graduation Year: '.$_POST['graduation_year']);
+    fwrite($handle, "\n".'GPA: '.$_POST['gpa']."\n");
+    fwrite($handle, "\n".'Challenge Question Response: '.$_POST['message']);
+    fclose($handle);
+
     //Create applicant folder on google drive
     $folderMetadata = new Google_Service_Drive_DriveFile(array(
         'name' => $studentName,
@@ -67,7 +96,6 @@ function createApplicantFolder($driveService, $parentFolderId, $studentName)
         ob_end_flush();
         break;
     }
-
     //Upload resume to google drive
     $fileMetadata = new Google_Service_Drive_DriveFile(array(
         'name' => 'resume.pdf',
@@ -79,8 +107,22 @@ function createApplicantFolder($driveService, $parentFolderId, $studentName)
         'mimeType' => 'application/pdf',
         'uploadType' => 'multipart',
         'fields' => 'id'));
+    //Upload application txt to google drive
+
+    $fileMetadata = new Google_Service_Drive_DriveFile(array(
+        'name' => $application_file,
+        'parents' => array($folder->id)
+    ));
+    $content = file_get_contents($application_file);
+    $file = $driveService->files->create($fileMetadata, array(
+        'data' => $content,
+        'mimeType' => 'text/plain',
+        'uploadType' => 'multipart',
+        'fields' => 'id'));
+
 
     //Redirect
+
     ob_start();
     echo '
     <script type="text/javascript">
@@ -88,5 +130,6 @@ function createApplicantFolder($driveService, $parentFolderId, $studentName)
     window.location = "http://otcr.illinois.edu/";
     </script>';
     ob_end_flush();
+
 }
 ?>
